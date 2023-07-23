06fdcde9b1ba2cd0dacabadc9c0c5b78660d9eb6
hayat711 <71981169+hayat711@users.noreply.github.com>
2023-07-23 13:38:45 +0900

    graphql module integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,50 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ThreadModule } from './thread/thread.module';
 import { ReplyModule } from './reply/reply.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 
 @Module({
-    imports: [PrismaModule, ConfigModule, AuthModule, UserModule, ThreadModule, ReplyModule],
+    imports: [
+        PrismaModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        AuthModule,
+        UserModule,
+        ThreadModule,
+        ReplyModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            sortSchema: true,
+            buildSchemaOptions: {
+                dateScalarMode: 'isoDate',
+            },
+            autoTransformHttpErrors: false,
+            includeStacktraceInErrorResponses: false,
+            formatError: (error: GraphQLError) => {
+                const graphQLFormattedError: GraphQLFormattedError = {
+                    //@ts-ignore
+                    message:
+                        error.extensions?.message ||
+                        error.message ||
+                        'Internal server error',
+                    statusCode:
+                        error.extensions?.statusCode ||
+                        error.extensions.code ||
+                        500,
+                    errors: error.extensions?.errors || {},
+                };
+                return graphQLFormattedError;
+            },
+            introspection: true,
+            playground: true,
+            csrfPrevention: false,
+        }),
+    ],
     controllers: [],
     providers: [],
 })
